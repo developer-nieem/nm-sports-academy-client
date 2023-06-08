260d70d258806c381d455a35e29067e0349410d6
developer-nieem <naimhossenwork@gmail.com>
2023-06-09 00:30:34 +0600

    impliment light dark theme

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,15 +1,30 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../page/Shared/NavBar/NavBar";
 import Footer from "../page/Shared/Footer/Footer";
+import { useState } from "react";
 
 const Main = () => {
-    return (
-        <div>
-            <NavBar></NavBar>
-            <Outlet></Outlet>
-            <Footer></Footer>
+    const [theme, setTheme] = useState('')
+  return (
+    <div data-theme={`${theme}`}>
+      <NavBar></NavBar>
+      <div className=" fixed top-96 z-10">
+        <div className="btn-group btn-group-vertical">
+          <button
+            onClick={() => setTheme("dark")}
+            className="btn bg-black hover:bg-black text-white"
+          >
+            Dark
+          </button>
+          <button onClick={() => setTheme("light")} className="btn">
+            Light
+          </button>
         </div>
-    );
+      </div>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Main;
--- src/page/Home/Home/Home.jsx
+++ src/page/Home/Home/Home.jsx
@@ -1,18 +1,22 @@
+
 import Banner from "../Banner";
 import Classes from "../Classes/Classes";
 import ComingMatch from "../ComingMatch";
 import Instructors from "../Instructors/Instructors";
 
 const Home = () => {
-    return (
-        <div>
-            <Banner></Banner>
-            <Classes></Classes>
-            <Instructors></Instructors>
 
-            <ComingMatch></ComingMatch>
-        </div>
-    );
+    
+  return (
+    <div >
+      <Banner></Banner>
+      <Classes></Classes>
+
+    
+      <Instructors></Instructors>
+      <ComingMatch></ComingMatch>
+    </div>
+  );
 };
 
 export default Home;
